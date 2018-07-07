e2ec61dd3a66cda27ccb76ffa639e0b9189d81b5
Steffen Klee <steffen.klee@gmail.com>
2018-07-07 03:03:23 +0200

    Added MariaDB client library find script

---
+++ FindMariaDBClient.cmake
@@ -0,0 +1,48 @@
+# MIT License
+#
+# Copyright (c) 2018 The ViaDuck Project
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+# - Try to find MariaDB client library. This matches both the "old" client library and the new C connector.
+# Once found this will define
+#  MariaDBClient_FOUND - System has MariaDB client library
+#  MariaDBClient_INCLUDE_DIRS - The MariaDB client library include directories
+#  MariaDBClient_LIBRARIES - The MariaDB client library
+
+# includes
+find_path(MariaDBClient_INCLUDE_DIR
+        NAMES mysql.h
+        PATH_SUFFIXES mariadb mysql
+        )
+
+# library
+find_library(MariaDBClient_LIBRARY
+        NAMES mariadbclient mysqlclient
+        PATH_SUFFIXES mariadb mysql
+        )
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(MariaDBClient DEFAULT_MSG MariaDBClient_LIBRARY MariaDBClient_INCLUDE_DIR)
+
+mark_as_advanced(MariaDBClient_INCLUDE_DIR MariaDBClient_LIBRARY)
+
+set(MariaDBClient_LIBRARIES ${MariaDBClient_LIBRARY})
+set(MariaDBClient_INCLUDE_DIRS ${MariaDBClient_INCLUDE_DIR})
